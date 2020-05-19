a67717fb758d1e450a1217688fb493463c344608
fengleifengdh <540480452@qq.com>
2020-05-20 00:11:13 +0800

    test

--- src/main/java/top/fengleifeng/question/Num4.java
+++ src/main/java/top/fengleifeng/question/Num4.java
@@ -9,14 +9,19 @@ public class Num4 {
     public static void main(String[] args) {
         int[] numOne = Arrays.copyOf(Constants.IntSortOne, Constants.IntSortOne.length);
         int[] numTwo = Arrays.copyOf(Constants.IntSortTwo, Constants.IntSortTwo.length);
-
+        Constants.printArr(numOne);
+        System.out.println();
+        Constants.printArr(numTwo);
         Num4 num4 = new Num4();
-//        System.out.println(num4.findMedianSortedArrays(numOne, numTwo));
         System.out.println(num4.ffindMedianSortedArrays1(numOne, numTwo));
 
-        int[] one = new int[]{1, 3};
-        int[] two = new int[]{2};
+        int[] one1 = new int[]{1, 3};
+        int[] two1 = new int[]{2};
+        int[] one = new int[]{0, 0};
+        int[] two = new int[]{0, 0};
+//        System.out.println(num4.findMedianSortedArrays(one1, two1));
 //        System.out.println(num4.findMedianSortedArrays(one, two));
+//        System.out.println(num4.findMedianSortedArrays(numOne, numTwo));
 
     }
 
@@ -51,58 +56,73 @@ public class Num4 {
 //        int left = (sum + 1) / 2;
 //        int right = (sum + 2) / 2;
 //        if (left == right) {
-//            result = Double.valueOf(findNum(nums1, nums1Length / 2, nums2, nums2Length / 2, left) + "");
+//            result = Double.valueOf(findMinK(nums1, 0, nums1Length - 1, nums2, 0, nums2Length - 1, left) + "");
 //        } else {
-//            double one = Double.valueOf(findNum(nums1, nums1Length / 2, nums2, nums2Length / 2, left) + "");
-//            double two = Double.valueOf(findNum(nums1, nums1Length / 2, nums2, nums2Length / 2, right) + "");
+//            double one = Double.valueOf(findMinK(nums1, 0, nums1Length - 1, nums2, 0, nums2Length - 1, left) + "");
+//            double two = Double.valueOf(findMinK(nums1, 0, nums1Length - 1, nums2, 0, nums2Length - 1, right) + "");
+//
 //            result = (one + two) / 2;
 //        }
 //        return result;
+//    }
+//
+//    public static double findMinK(int[] a, int lowA, int highA, int[] b, int lowB, int highB, int k) {
+//
+//        int len1 = highA - lowA + 1;
+//        int len2 = highB - lowB + 1;
+//
+//        if (len1 > len2) {
+//            return findMinK(b, lowB, highB, a, lowA, highA, k);
+//        }
+//        if (len1 == 0) {
+//            return (double)b[lowB+k-1];
+//        }
+//
+//        int na = Math.min(k/2, len1);
+//        int nb = k-na;
+//        if (a[na+lowA-1] < b[nb+lowB-1]) {
+//            return findMinK(a, na+lowA, highA, b, lowB, nb+lowB-1, k-na);
+//        } else if (a[na+lowA-1] == b[nb+lowB-1]) {
+//            return (double)a[na+lowA-1];
+//        } else {
+//            return findMinK(b, nb+lowB, highB, a, lowA, na+lowA-1, k-nb);
+//        }
+//
 //    }
 
 
     /**
      * 找第k个数
      *
-     * @param nums1
-     * @param i
-     * @param nums2
-     * @param j
-     * @param k
      * @return
      */
-//    public int findNum(int[] nums1, int i, int[] nums2, int j, int k) {
-//        int temp1 = i, temp1Last = i + 1;
-//        int temp2 = j, temp2Last = j + 1;
+//    public int findNum(int[] nums1, int start1, int end1, int[] nums2, int start2, int end2, int k) {
 //
-//        int step = (k / 3) > 0 ? k / 3 : 1;//步距
-//        int sum = 0;
-//        while (sum != k) {
-//            if (nums1[temp1] < nums2[temp2]) {
-//                sum = temp1Last + sum;
-//
-//                if (sum == k) {
-//                    return nums1[temp1];
-//                }
-//
-//                temp1 = temp1 + step;
-//                temp2 = (temp2 + 1 - step) > 0 ? temp2 - step : 0;
-//            } else {
-//                sum = temp2Last + sum;
-//                temp2 = temp2 + step;
-//                temp1 = (temp1 + 1 - step) > 0 ? temp1 - step : 0;
-//                if (sum == k) {
-//                    return nums2[temp2];
-//                }
-//            }
-//
-//            if (step / 2 > 0) {
-//                step = step / 2;
-//            }
-//            temp1Last = step;
-//            temp2Last = step;
+//        if (start1 == nums1.length) {
+//            return nums2[start2 + k];
 //        }
-//        return -1;
+//        if (start2 == nums2.length) {
+//            return nums1[start1 + k];
+//        }
+//        if (k <= 1) {
+//            return nums1[start1] < nums2[start2] ? nums1[start1] : nums2[start2];
+//        }
+//        int step = k / 2;
+//        int comp1 = (start1 + step) >= nums1.length ? nums1.length - 1 : (start1 + step);
+//        int comp2 = (start2 + step) >= nums2.length ? nums2.length - 1 : (start2 + step);
+//        if (nums1[comp1] < nums2[comp2]) {
+//            if (comp1 + 1 >= nums1.length) {
+//                return findNum(nums1, nums1.length, end1, nums2, start2, end2, k - (end1 - start1 + 1));
+//            } else {
+//                return findNum(nums1, comp1 + 1, end1, nums2, start2, end2, k - (comp1 - start1 + 1));
+//            }
+//        } else {
+//            if (comp2 + 1 >= nums2.length) {
+//                return findNum(nums1, start1, end1, nums2, nums2.length, end2, k - (end2 - start2 + 1));
+//            } else {
+//                return findNum(nums1, start1, end1, nums2, comp1 + 1, end2, k - (comp1 - start2 + 1));
+//            }
+//        }
 //    }
 
 }
