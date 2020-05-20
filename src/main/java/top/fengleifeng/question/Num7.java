142e9f566a815643d3eb9f3d97f2722746b84056
fengleifengdh <540480452@qq.com>
2020-05-20 22:41:02 +0800

    test

--- src/main/java/top/fengleifeng/question/Num7.java
+++ src/main/java/top/fengleifeng/question/Num7.java
@@ -1,10 +1,38 @@
 package top.fengleifeng.question;
 
-/**
- * @program: leetcode-test
- * @description:
- * @author: fengleifeng
- * @create: 2020-05-20 10:20
- **/
 public class Num7 {
+    public static void main(String[] args) {
+        System.out.println(new Num7().reverse(1463847412));
+    }
+
+    public int reverse(int x) {
+        int result = 0;
+        boolean flag = false;
+        //
+        if(x<=-2147483648){
+            return 0;
+        }
+        if (x < 0) {
+            flag = true;
+            x = -x;
+        }
+        for (int i = 0; i < Integer.MAX_VALUE; i++) {
+            int k = x % 10;
+
+            //判断是否溢出
+            if (result > Integer.MAX_VALUE / 10 || (result == Integer.MAX_VALUE / 10 && k > 7))
+                return 0;
+
+
+            result = result * 10 + k;
+            if (x < 10) {
+                if (flag) {
+                    result = result * -1;
+                }
+                return result;
+            }
+            x = x / 10;
+        }
+        return -1;
+    }
 }
