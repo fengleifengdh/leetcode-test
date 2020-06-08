2f8c59c58232a3bb6cc6e2bde9e462da555e5bd4
冯磊峰 <fengleifeng@meituan.com>
2020-06-08 10:17:25 +0800

    00

--- src/main/java/top/fengleifeng/util/Constants.java
+++ src/main/java/top/fengleifeng/util/Constants.java
@@ -5,13 +5,13 @@ import top.fengleifeng.总结.树.TreeNode;
 import java.util.List;
 
 public class Constants {
-    public static final int[][] IntTwoArrSortOne = {{1,2},{3,4},{7,3},{3,2},{88,8}};
+    public static final int[][] IntTwoArrSortOne = {{1, 2}, {3, 4}, {7, 3}, {3, 2}, {88, 8}};
     public static final int[] IntSortOne = {1, 3, 4, 5, 6, 7, 8, 9, 10, 14};// length 10
     public static final int[] IntSortTwo = {1, 4, 6, 6, 7, 7, 8, 8, 10, 11};// length 10
 
     public static final int[] IntNotSortOne = {2, 5, 16, 21, 51, 10, 1, 11, 4, 23};// length 10
     public static final int[] IntNotSortTwo = {3, 1, 511, 23, 5, 31, 6, 65, 2, 11};// length 10
-    public static final int[][] IntSortThree = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};// length 10
+    public static final int[][] IntSortThree = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};// length 10
 
     public static TreeNode treeNode;
 
@@ -58,6 +58,16 @@ public class Constants {
         System.out.println("------");
     }
 
+    public static void printArrs(char[][] intArr) {
+        for (int i = 0; i < intArr.length; i++) {
+            for (int j = 0; j < intArr[0].length; j++) {
+                System.out.print("[" + intArr[i][j] + "],");
+            }
+            System.out.println("");
+        }
+        System.out.println("------");
+    }
+
     public static int[][] copy2Arr(int[][] source) {
         int[][] target = new int[source.length][source[0].length];
         for (int i = 0; i < source.length; i++) {
@@ -72,7 +82,7 @@ public class Constants {
     public static void pringtLinkedList(List<List<Integer>> source) {
         for (int i = 0; i < source.size(); i++) {
             for (int j = 0; j < source.get(i).size(); j++) {
-                System.out.print(source.get(i).get(j)+",");
+                System.out.print(source.get(i).get(j) + ",");
             }
             System.out.println();
         }
